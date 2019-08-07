02300b82ba7003c4e1b19f23ae76c9930e4cfcfb
András Nyiscsák <andras.nyiscsak@outlook.com>
2019-08-07 23:18:00 +0200

    Initialized the project

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HideMenu")]
+[assembly: AssemblyDescription("Hides the menu of Visual Studio 2019. Use the alt key or ctrl+q to show it.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Andras Nyiscsak")]
+[assembly: AssemblyProduct("HideMenu")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
